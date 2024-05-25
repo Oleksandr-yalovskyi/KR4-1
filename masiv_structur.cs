e5f9fc5a8d7f3e2eb2579b59c5a249561501f300
Oleksandr-yalovskyi <77542534+Oleksandr-yalovskyi@users.noreply.github.com>
2024-05-25 20:29:10 +0200

    Update masiv_structur.cs

--- masiv_structur.cs
+++ masiv_structur.cs
@@ -1,38 +1,38 @@
 using System;
 
 namespace KR{
-    struct Plane{ // створення структури з параметрами рейсу
-        public string flight; // ім'я рейсу
-        public string destination; // місце призначення
-        public int seats; // кількість вільних місць
+    struct Plane{ // creating a structure with flight parameters
+        public string flight; // flight name
+        public string destination; // destination
+        public int seats; // number of available seats
 
-        public Plane(string flight, string destination, int seats){ // конструктор структури
+        public Plane(string flight, string destination, int seats){ // structure constructor
             this.flight = flight;
             this.destination = destination;
             this.seats = seats;
         }
-        public void WritePlane(int num){ // вивід параметрів
+        public void WritePlane(int num){ // output parameters
             Console.WriteLine($"Flight №{num + 1} : {flight};");
             Console.WriteLine($"Destination: {destination};");
-            Console.WriteLine($"Number of available seats {seats};");
+            Console.WriteLine($"Number of available seats: {seats};");
         }
     }
-    struct Date{ // створення структури з датою рейса
-        public int day, month, hour, minute; // день, місяц, година та хвилини відправлення
+    struct Date{ // creating a structure with the flight date
+        public int day, month, hour, minute; // day, month, hour and minutes of departure
 
-        public Date(int day, int month, int hour, int minute){ // конструктор структури
+        public Date(int day, int month, int hour, int minute){ // structure constructor
             this.day = day;
             this.month = month;
             this.hour = hour;
             this.minute = minute;
         }
-        public void WriteDate(){ // вивід параметрів
+        public void WriteDate(){ // output parameters
             Console.WriteLine($"Departure date and time: {day}.{month} {hour}:{minute};");
         }
     }
     class Program{
         static void Main(string[] args){
-            Plane[] flights = new Plane[] { // запевнення масиву структур параметрами
+            Plane[] flights = new Plane[] { // initializing an array of structures with parameters
                 new Plane() {flight = "GH-327", destination = "Kyiv", seats = 10},
                 new Plane() {flight = "JD-567", destination = "Lviv", seats = 1},
                 new Plane() {flight = "BS-603", destination = "Odessa", seats = 15},
@@ -41,7 +41,7 @@ namespace KR{
                 new Plane() {flight = "US-517", destination = "New-York", seats = 0},
                 new Plane() {flight = "GH-342", destination = "Kyiv", seats = 5},
             };
-            Date[] dates = new Date[] { // запевнення масиву структур параметрами
+            Date[] dates = new Date[] { // initializing an array of structures with parameters
                 new Date() {day = 14, month = 05, hour = 12, minute = 10},
                 new Date() {day = 15, month = 05, hour = 8, minute = 50},
                 new Date() {day = 14, month = 05, hour = 10, minute = 40},
@@ -52,7 +52,7 @@ namespace KR{
             };
             Date pDate;
 
-            Console.Write("Enter your destination point: "); // введення користувачем запиту на рейс
+            Console.Write("Enter your destination point: "); // user input for the flight request
             string pdestination = Console.ReadLine();
 
             Console.Write("Enter how many tickets you need: ");
@@ -71,7 +71,7 @@ namespace KR{
             Console.WriteLine("Here are some suitable flights by the end of this month:");
             bool b = false;
 
-            for (int j = 0; j < flights.Length; j++){ // перевірка на підходящість рейсу
+            for (int j = 0; j < flights.Length; j++){ // checking for suitable flights
                 if (pdestination == flights[j].destination){
                     if (pDate.day <= dates[j].day && pDate.month == dates[j].month){
                         if (pDate.hour <= dates[j].hour && pDate.minute <= dates[j].minute){
@@ -85,7 +85,7 @@ namespace KR{
                 }
             }
 
-            if (b == false){ // виввід у випадку, якщо рейс не знайдено
+            if (b == false){ // output if no flights are found
                 Console.WriteLine("Sorry, but there are no suitable flights");
             }
         }
